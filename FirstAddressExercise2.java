94821c59089f75241cbc151ab115d22661a2c82a
Larry Fernandez <laryfernandez12@gmail.com>
2024-09-20 08:15:11 +0700

    Create FirstAddressExercise2.java

---
+++ FirstAddressExercise2.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise2 {
+public static void main(String[] args) {
+System.out.println("First Java Application");
+
+System.out.println("XYZ Company");
+displayAddress();
+System.out.println("Company: Apple Inc.");
+}
+
+public static void displayAddress() {
+System.out.println("8900 U.S. Hwy 14");
+System.out.println("Crystal Lake, IL 60014");
+      }
+}
